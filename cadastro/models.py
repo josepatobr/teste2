1b42186b5d61cd8630fb30abf51323838ce896e2
dkshs <88355080+dkshs@users.noreply.github.com>
2024-12-02 17:43:20 -0300

    refactor: update code format

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Pessoas(models.Model):
     nome = models.CharField(max_length=200)
     email = models.EmailField(max_length=1000, null=True)
--- cadastro/urls.py
+++ cadastro/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.cadastro, name="cadastro")
-  
+    path("", views.cadastro, name="cadastro"),
 ]
